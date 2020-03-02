2efed656c01e2837caae5a598c1e857ef3e92889
DBuckley0126 <DannyBoy.0126>
2020-03-02 00:39:24 +0000

    Change seed file

--- config/initializers/perform_seed.rb
+++ config/initializers/perform_seed.rb
@@ -1,10 +1,10 @@
-computer_user = User.find_by(sub: "backend|5e45d67f1ba0ebb439e98")
+# computer_user = User.find_by(sub: "backend|5e45d67f1ba0ebb439e98")
 
-if computer_user
-  User.destroy(computer_user.id)
-end
+# if computer_user
+#   User.destroy(computer_user.id)
+# end
 
-User.create(given_name: "Game", family_name: "Controller", locale: "en-GB", sub: "backend|5e45d67f1ba0ebb439e98", uuid: "F04gDzK3LVhzPJCpzKqIWw")
+# User.create(given_name: "Game", family_name: "Controller", locale: "en-GB", sub: "backend|5e45d67f1ba0ebb439e98", uuid: "F04gDzK3LVhzPJCpzKqIWw")
 
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Game.destroy_all
-Unit.destroy_all
-Spawner.destroy_all 
-Turn.destroy_all
+# Game.destroy_all
+# Unit.destroy_all
+# Spawner.destroy_all 
+# Turn.destroy_all
 
-Game.create(uuid: "a9bb9988-3615-9d2a-8358-625f80a21d49", host_user_id: 3, host_user_type: User, host_user_colour: "#3ca832", join_user_colour: "#3ca832")
+# Game.create(uuid: "a9bb9988-3615-9d2a-8358-625f80a21d49", host_user_id: 3, host_user_type: User, host_user_colour: "#3ca832", join_user_colour: "#3ca832")
+
+computer_user = User.find_by(sub: "backend|5e45d67f1ba0ebb439e98")
+
+if computer_user
+  User.destroy(computer_user.id)
+end
+
+User.create(given_name: "Game", family_name: "Controller", locale: "en-GB", sub: "backend|5e45d67f1ba0ebb439e98", uuid: "F04gDzK3LVhzPJCpzKqIWw")
+
